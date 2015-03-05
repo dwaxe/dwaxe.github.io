139b6cc97684c7d9e41efae4b6cffaafbebd8ce8
Darwin <DarwinWhy@gmail.com>
2015-03-05 13:13:26 +0000

    clicking up moves up

--- scroller/js/scrollocue.js
+++ scroller/js/scrollocue.js
@@ -82,21 +82,20 @@
 
         // Set cursor at first line
         setCursor(0);
-        // Set up element click handling
-        $("p").click(function () {
-            console.log($(this).offset());
-            console.log($(lines[cursor]).offset());
-            if ($(this).offset().top < $(lines[cursor]).offset().top) {
-                decreaseCursor(2);
-            }
-        });
+
         // Set up key press handling.
         $(window.document).keydown(handleKeyDown);
-        container.click(function () {
+
+        // Click handling
+        container.click(function (event) {
+            // If clicking above current active element, scroll up by one
+            if (event.pageY < $(lines[cursor]).offset().top) { return decreaseCursor(); }
             // If at bottom, return to top on click
-            if (cursor === lines.length - 1) { setCursor(0); return; }
+            if (cursor === lines.length - 1) { return setCursor(0); }
+            // Else just scroll down
             increaseCursor();
         });
+
         // Mobile swiping
         $(container).swipe({
             swipeUp: function () {
@@ -107,7 +106,8 @@
                 console.log('swiped down');
                 decreaseCursor();
             },
-            threshold: 50
+            threshold: 50,
+            cancelThreshold: 15
         });
 
     };
@@ -119,4 +119,4 @@
     };
 
 
-}) (window, $);
+}(window, $));
